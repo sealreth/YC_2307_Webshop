6d9970ef1936de7b759b229363c86089455a93cf
Rogier van Schaik <rogiervschaik@hotmail.com>
2023-08-21 09:10:21 +0200

    voor martijn

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -36,3 +36,63 @@ function getShoppingCartEntriesCount(accountId) {
 window.addEventListener("load", (event) => {
     getShoppingCartEntriesCount(1);
 });
+// Inside the window.addEventListener("load", ...) block
+
+// Function to update the shopping cart content in the dropdown
+function updateShoppingCartDropdown(cartItems) {
+    const cartContent = document.getElementById("shopping-cart-content");
+    cartContent.innerHTML = "";
+
+    if (cartItems.length === 0) {
+        cartContent.innerHTML = "<p class='dropdown-item-text'>Your cart is empty</p>";
+    } else {
+        for (const item of cartItems) {
+            cartContent.innerHTML += `
+                <div class="dropdown-item">
+                    ${item.quantity} x ${item.productName} - &euro;${item.totalPrice.toFixed(2)}
+                    <button class="delete-button" data-product-id="${item.productId}">Delete</button>
+                </div>
+                <div class="dropdown-divider"></div>
+            `;
+        }
+
+        // Add a "Checkout" button
+        cartContent.innerHTML += `
+            <a class="dropdown-item" href="#!">Checkout</a>
+        `;
+
+        // Add event listeners to delete buttons
+        const deleteButtons = document.querySelectorAll(".delete-button");
+        deleteButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                const productId = button.getAttribute("data-product-id");
+                deleteCartItem(productId);
+            });
+        });
+    }
+}
+function deleteCartItem(productId) {
+    // Add your fetch logic here to delete the cart item
+    // Update the cart content after successful deletion
+}
+
+// Function to show/hide the shopping cart dropdown
+function toggleShoppingCartDropdown() {
+    const cartDropdown = document.getElementById("shopping-cart-dropdown");
+    cartDropdown.classList.toggle("show");
+}
+
+// Add event listener to the cart button to toggle the dropdown
+const cartButton = document.getElementById("shopping-cart-button");
+cartButton.addEventListener("click", () => {
+    toggleShoppingCartDropdown();
+});
+
+// Example cart items for testing
+const exampleCartItems = [
+    { productName: "Product 1", quantity: 2, totalPrice: 25.00 },
+    { productName: "Product 2", quantity: 1, totalPrice: 15.00 }
+];
+
+// Call this function to populate the shopping cart dropdown with example items
+updateShoppingCartDropdown(exampleCartItems);
