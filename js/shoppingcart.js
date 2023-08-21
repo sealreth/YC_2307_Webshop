611b706f3547ea9c5e1726b8351818c594ac95fb
Martijn Hiemstra <info@greenhighway.nl>
2023-08-21 09:44:50 +0200

    Updated shoppingcart

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -15,65 +15,58 @@ function addShoppingCartEntry(accountId, productId, amount) {
         if (success) {
             displaySuccessMessage("Product is toegevoegd")        
 
-            getShoppingCartEntriesCount(accountId);
+            getShoppingCartEntries(accountId);
         }
     })
 }
 
-function getShoppingCartEntriesCount(accountId) {
-    fetch(BACKEND_URL + '/api/shoppingcart/' + accountId + '/entry/count')
-        .then(response => response.text())
-        .then(count => {
-            let shoppingCartCountElement = document.getElementById('shoppingcart-count');
-            if (!!shoppingCartCountElement) {
-                shoppingCartCountElement.innerText = count;
+function getShoppingCartEntries(accountId) {
+    fetch(BACKEND_URL + '/api/shoppingcart/' + accountId + '/entries')
+        .then(response => response.json())
+        .then(cartItems => {
+
+            const cartContent = document.getElementById("shopping-cart-content");
+            cartContent.innerHTML = "";
+
+            document.getElementById('shoppingcart-count').innerText = cartItems.length;
+
+            if (cartItems.length === 0) {
+                cartContent.innerHTML = "<p class='dropdown-item-text'>Your cart is empty</p>";
             } else {
-                shoppingCartCountElement.innerText = 0;
+                for (const item of cartItems) {
+                    cartContent.innerHTML += `
+                        <div class="dropdown-item">
+                            ${item.amount} x ${item.product.naam} - &euro; ${item.product.prijs}
+                            <button class="delete-button" onclick="deleteCartItem(${item.id})">Delete</button>
+                        </div>
+                        <div class="dropdown-divider"></div>
+                    `;
+                }
+
+                // Add a "Checkout" button
+                cartContent.innerHTML += `
+                    <a class="dropdown-item" href="#!">Checkout</a>
+                `;
             }
         })
 }
 
 window.addEventListener("load", (event) => {
-    getShoppingCartEntriesCount(1);
+    getShoppingCartEntries(1);
 });
 // Inside the window.addEventListener("load", ...) block
 
-// Function to update the shopping cart content in the dropdown
-function updateShoppingCartDropdown(cartItems) {
-    const cartContent = document.getElementById("shopping-cart-content");
-    cartContent.innerHTML = "";
-
-    if (cartItems.length === 0) {
-        cartContent.innerHTML = "<p class='dropdown-item-text'>Your cart is empty</p>";
-    } else {
-        for (const item of cartItems) {
-            cartContent.innerHTML += `
-                <div class="dropdown-item">
-                    ${item.quantity} x ${item.productName} - &euro;${item.totalPrice.toFixed(2)}
-                    <button class="delete-button" data-product-id="${item.productId}">Delete</button>
-                </div>
-                <div class="dropdown-divider"></div>
-            `;
-        }
-
-        // Add a "Checkout" button
-        cartContent.innerHTML += `
-            <a class="dropdown-item" href="#!">Checkout</a>
-        `;
-
-        // Add event listeners to delete buttons
-        const deleteButtons = document.querySelectorAll(".delete-button");
-        deleteButtons.forEach(button => {
-            button.addEventListener("click", () => {
-                const productId = button.getAttribute("data-product-id");
-                deleteCartItem(productId);
-            });
-        });
-    }
-}
-function deleteCartItem(productId) {
+function deleteCartItem(entryId) {
     // Add your fetch logic here to delete the cart item
     // Update the cart content after successful deletion
+    fetch(BACKEND_URL + '/api/shoppingcart/entry/' + entryId, {
+        method: 'delete'
+    })
+    .then(() => {
+        displaySuccessMessage("Product is verwijderd uit winkelwagen") 
+
+        getShoppingCartEntries(accountId);
+    })
 }
 
 // Function to show/hide the shopping cart dropdown
@@ -88,11 +81,3 @@ cartButton.addEventListener("click", () => {
     toggleShoppingCartDropdown();
 });
 
-// Example cart items for testing
-const exampleCartItems = [
-    { productName: "Product 1", quantity: 2, totalPrice: 25.00 },
-    { productName: "Product 2", quantity: 1, totalPrice: 15.00 }
-];
-
-// Call this function to populate the shopping cart dropdown with example items
-updateShoppingCartDropdown(exampleCartItems);
